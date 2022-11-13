f34c36a1592e11c0947ccf7768263b1dd00a790c
me3za <medkrix1@gmail.com>
2022-11-13 23:38:12 +0100

    reformatted all commands using prettier and cleaned the code a bit and handled a lot more exceptions so the bot dosen't shut down on commands and

--- commands/creator.js
+++ commands/creator.js
@@ -1,10 +1,17 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data : new SlashCommandBuilder()
-    .setName('creator')
-    .setDescription('Names the creator of the bot'),
-    execute : async (interaction) => {
-        return await interaction.reply('The sexy madlad that me3za is 🥺.')
-    }
-}
+	data: new SlashCommandBuilder()
+		.setName('creator')
+		.setDescription('Names the creator of the bot'),
+	execute: async (interaction) => {
+		try {
+			return await interaction.reply('The sexy madlad that me3za is 🥺.');
+		} catch (err) {
+			console.log(err);
+			return await interaction.reply(
+				'Command failed :( contact me3za#4854 please.'
+			);
+		}
+	},
+};
--- commands/status.js
+++ commands/status.js
@@ -1,10 +1,17 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data : new SlashCommandBuilder()
-    .setName('status')
-    .setDescription('status of the bot'),
-    execute : async (interaction) => {
-        return await interaction.reply('☑️ Up and running!')
-    }
-}
+	data: new SlashCommandBuilder()
+		.setName('status')
+		.setDescription('status of the bot'),
+	execute: async (interaction) => {
+		try {
+			return await interaction.reply('☑️ Up and running!');
+		} catch (err) {
+			console.log(err);
+			return await interaction.reply(
+				'Command failed :( contact me3za#4854 please.'
+			);
+		}
+	},
+};
