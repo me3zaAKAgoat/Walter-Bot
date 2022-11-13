3dcd32a09796595e840bf963d01858730a798bef
me3za <medkrix1@gmail.com>
2022-11-14 00:38:59 +0100

    fixed twitter scraping re post of same tweet

--- events/ready.js
+++ events/ready.js
@@ -4,69 +4,92 @@ const LatestScrape = require('../models/latestScrape');
 const cron = require('cron');
 const { findById } = require('../models/movie');
 
-const appOnlyClient = new TwitterApi(process.env.TWT_BEARER_TOKEN);
+const Twitterclient = new TwitterApi(process.env.TWT_BEARER_TOKEN);
 
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute : async (client) => {
+	execute: async (client) => {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 
 		const scrapeTwitterNews = async () => {
-			const newsChannelId = '1040725565235273808';
-			const newsChannel = client.channels.cache.get(newsChannelId)
-	
-			const keywords = ['BREAKING', 'NEWS', 'ANNOUNCEMENT'];
-			const latestScrapeQuery = await LatestScrape.find({});
-			const latestScrape = latestScrapeQuery[0].latestScrape;
-	
-			if (!latestScrape || !(latestScrape instanceof Date))
-			{
-				console.log('couldnt collect latest scrape document')
-				return ;
-			}
-		
-			let currentFetches = 0; // two variables used to gate how many fetches for tweets will occur to prevent breaking rate limit
-			const maximumFetches = 3;
-		
-			const user = await appOnlyClient.v2.userByUsername('Crunchyroll');
-			const userId = user.data.id;
-			const tweetsPage = await appOnlyClient.v2.userTimeline(userId, { exclude : 'replies', exclude : 'retweets', 'tweet.fields' : ['created_at']})
-			
-			let tweetsArr = tweetsPage.data.data;
-			
-			while (new Date(tweetsArr[tweetsArr.length - 1].created_at) > latestScrape && currentFetches < maximumFetches) // while the oldest fetched tweet is younger than my latest scrape
-			{
-				await tweetsPage.fetchNext();
-				tweetsArr = tweetsPage.data.data;
-				currentFetches++;
-			}
-	
-			for (const tweet of tweetsArr)
-			{		
-				let found = false;
-				for (const keyword of keywords)
-				{
-					found = tweet.text.toLowerCase().includes(keyword.toLowerCase());
-					if (found)
-					{
-						console.log(tweet.text)
-						newsChannel.send(`https://twitter.com/Crunchyroll/status/${tweet.id}`);
-						console.log('message sent');
-						break;
-					}
-					found = false;
+			try {
+				const newsChannelId = '1040725565235273808';
+				const newsChannel = client.channels.cache.get(newsChannelId);
+
+				const keywords = ['BREAKING', 'NEWS', 'ANNOUNCEMENT'];
+				const latestScrapeQuery = await LatestScrape.find({});
+				const latestScrape = latestScrapeQuery[0].latestScrape;
+
+				if (!latestScrape || !(latestScrape instanceof Date)) {
+					console.log('couldnt collect latest scrape document');
+					return;
 				}
+
+				let currentFetches = 0; // two variables used to gate how many fetches for tweets will occur to prevent breaking rate limit
+				const maximumFetches = 3;
+
+				const user = await Twitterclient.v2.userByUsername('Crunchyroll');
+				const userId = user.data.id;
+				const tweetsPage = await Twitterclient.v2.userTimeline(userId, {
+					exclude: 'replies',
+					exclude: 'retweets',
+					'tweet.fields': ['created_at'],
+				});
+
+				let tweetsArr = tweetsPage.data.data;
+				if (tweetsArr.length == 0) {
+					console.log('no tweets were fetched');
+					return;
+				}
+				tweetsArr = tweetsArr.filter(
+					(tweet) => new Date(tweet.created_at) > latestScrape
+				);
+				while (
+					new Date(tweetsArr[tweetsArr.length - 1].created_at) > latestScrape &&
+					currentFetches < maximumFetches
+				) {
+					// while the oldest fetched tweet is younger than my latest scrape
+					await tweetsPage.fetchNext();
+					tweetsArr = tweetsPage.data.data;
+					currentFetches++;
+				}
+
+				tweetsArr.reverse();
+				for (const tweet of tweetsArr) {
+					let found = false;
+					for (const keyword of keywords) {
+						found = tweet.text.toLowerCase().includes(keyword.toLowerCase());
+						if (found) {
+							console.log(tweet.text);
+							newsChannel.send(
+								`https://twitter.com/Crunchyroll/status/${tweet.id}`
+							);
+							console.log('message sent');
+							break;
+						}
+						found = false;
+					}
+				}
+
+				currentDate = new Date();
+
+				await LatestScrape.findByIdAndUpdate(
+					latestScrapeQuery[0]._id.toString(),
+					{
+						latestScrape: currentDate,
+					}
+				);
+			} catch (err) {
+				console.log(err);
 			}
-			
-			await LatestScrape.findByIdAndUpdate(latestScrapeQuery[0]._id.toString(), {
-				latestScrape : new Date()
-			})
+		};
 
-		}
-
-		let scheduledScrape = new cron.CronJob('00 06,18 * * *', scrapeTwitterNews)
+		let scheduledScrape = new cron.CronJob(
+			'00 06,18,12,00 * * *',
+			scrapeTwitterNews
+		);
 
 		scheduledScrape.start();
-	}
-}
+	},
+};
