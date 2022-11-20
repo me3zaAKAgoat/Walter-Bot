178968ef8a4c6ae7d1f6c381258111b1c85a4015
El mehdi Choukri <echoukri@e1r2p5.1337.ma>
2022-11-20 20:31:38 +0100

    made the meme command functional with wallace vibrator and normified the resolutions of meme templates

--- commands/meme.js
+++ commands/meme.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Jimp = require('jimp');
-const fs = require('fs');
 const process = require('process');
 
 module.exports = {
@@ -20,41 +19,73 @@ module.exports = {
 				.setRequired(true)
 				.addChoices(
 					{ name: 'when competition', value: 'squidward' },
-					{ name: 'allo', value: 'wallace' }
+					{ name: 'allo', value: 'wallace' },
+					{ name: 'vibrator', value: 'vibrator' }
 				)
 		),
+
 	execute: async (interaction) => {
 		const type = interaction.options.getString('type');
 		if (type === 'squidward') {
 			try {
 				const content = interaction.options.getString('content');
 				await interaction.deferReply();
-				if (content.length > 60) {
+				if (content.length > 100) {
 					return await interaction.editReply({
 						content: '🚫 You have way too many characters to fit in this meme.',
 						ephemeral: true,
 					});
 				}
-				const wordsArr = content.split(' ');
-				let sanitizedText = '';
-				let lineLength = 0;
-				const maxLength = 20;
-				for (const word of wordsArr) {
-					if (lineLength < maxLength) {
-						sanitizedText += ' ' + word;
-						lineLength += 1 + word.length;
-					} else {
-						sanitizedText += '\n' + word;
-						lineLength = 0;
-					}
-				}
-				console.log(sanitizedText);
-				await writeToImage(sanitizedText, type).then(
-					await interaction.channel.send({
-						files: ['./commands/memes/tmp/output.jepg'],
+				writeMeme(type, content)
+					.then(async (meme) => {
+						await interaction.editReply({ files: [meme] });
 					})
-				);
-				return await interaction.editReply('done');
+					.catch((error) => console.log(error));
+			} catch (err) {
+				console.log(err);
+			}
+		} else if (type === 'wallace') {
+			try {
+				const content = interaction.options.getString('content');
+				await interaction.deferReply();
+				if (content.length > 100) {
+					return await interaction.editReply({
+						content: '🚫 You have way too many characters to fit in this meme.',
+						ephemeral: true,
+					});
+				}
+				writeMeme(type, content)
+					.then(async (meme) => {
+						await interaction.editReply({ files: [meme] });
+					})
+					.catch((error) => console.log(error));
+			} catch (err) {
+				console.log(err);
+			}
+		} else if (type === 'vibrator') {
+			try {
+				const content = interaction.options.getString('content');
+				await interaction.deferReply();
+				if (content.length > 100) {
+					return await interaction.editReply({
+						content: '🚫 You have way too many characters to fit in this meme.',
+						ephemeral: true,
+					});
+				}
+				writeMeme(type, content)
+					.then(async (meme) => {
+						await interaction.editReply({ files: [meme] });
+					})
+					.catch((error) => console.log(error));
+			} catch (err) {
+				console.log(err);
+			}
+		} else {
+			try {
+				await interaction.reply({
+					ephemeral: true,
+					content: 'This meme template does not exist',
+				});
 			} catch (err) {
 				console.log(err);
 			}
@@ -62,16 +93,38 @@ module.exports = {
 	},
 };
 
-const writeToImage = async (sanitizedText, type) => {
-	const img = await Jimp.read(`${process.cwd()}/commands/memes/${type}.jpg`);
+const writeMeme = async (type, content) => {
+	let image = await Jimp.read(`${process.cwd()}/commands/memes/${type}.jpg`);
 
-	Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+	let x = 1;
+	let y = 4;
+	let maxWidth = 400;
+	let maxHeight = 100;
+	let memeFont = Jimp.FONT_SANS_32_BLACK;
+	if (type === 'vibrator') memeFont = Jimp.FONT_SANS_16_BLACK;
+	if (type === 'wallace') {
+		x = 2;
+		y = 0;
+		maxWidth = 300;
+	}
+	await Jimp.loadFont(memeFont)
 		.then((font) => {
-			img.print(font, 22, 20, sanitizedText);
-			return img;
+			image.print(
+				font,
+				x,
+				y,
+				{
+					text: content,
+					alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+				},
+				maxWidth,
+				maxHeight
+			);
+			return image;
 		})
 		.then((image) => {
-			let file = `${process.cwd()}/commands/memes/tmp/output.${img.getExtension()}`;
+			let file = `${process.cwd()}/commands/memes/tmp/output.${image.getExtension()}`;
 			return image.write(file); // save
 		});
+	return `${process.cwd()}/commands/memes/tmp/output.${image.getExtension()}`;
 };
