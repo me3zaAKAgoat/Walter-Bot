2dd7e05e009984aa22a6ec01586398f7ec01deb6
me3za <medkrix1@gmail.com>
2022-12-08 13:11:02 +0100

    fixed formatting of meme templates

--- commands/meme.js
+++ commands/meme.js
@@ -8,105 +8,104 @@ module.exports = {
 		.setDescription(`makes a meme out of your input`)
 		.addStringOption((option) =>
 			option
+				.setName('type')
+				.setDescription('Select animal for photo')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'when Im in a competition', value: 'squidward' },
+					{ name: 'allo o_o', value: 'wallace' },
+					{ name: 'vibrator', value: 'vibrator' },
+					{ name: 'sir tat... w 3ad aji', value: 'sirtat' }
+					// { name: 'temptation button', value: 'trynotto' }
+				)
+		)
+		.addStringOption((option) =>
+			option
 				.setName('content')
 				.setDescription('the text that fills the meme')
 				.setRequired(true)
 		)
 		.addStringOption((option) =>
 			option
-				.setName('type')
-				.setDescription('Select animal for photo')
-				.setRequired(true)
-				.addChoices(
-					{ name: 'when competition', value: 'squidward' },
-					{ name: 'allo', value: 'wallace' },
-					{ name: 'vibrator', value: 'vibrator' }
-				)
+				.setName('content_more')
+				.setDescription('the text that fills the meme')
+				.setRequired(false)
 		),
-
 	execute: async (interaction) => {
-		const type = interaction.options.getString('type');
-		if (type === 'squidward') {
-			try {
+		try {
+			const type = interaction.options.getString('type');
+			await interaction.deferReply();
+			if (type === 'squidward') {
 				const content = interaction.options.getString('content');
-				await interaction.deferReply();
 				if (content.length > 100) {
 					return await interaction.editReply({
 						content: '🚫 You have way too many characters to fit in this meme.',
 						ephemeral: true,
 					});
 				}
-				writeMeme(type, content)
+				writeMeme(type, content, 1, 4)
 					.then(async (meme) => {
 						await interaction.editReply({ files: [meme] });
 					})
 					.catch((error) => console.log(error));
-			} catch (err) {
-				console.log(err);
-			}
-		} else if (type === 'wallace') {
-			try {
+			} else if (type === 'wallace') {
 				const content = interaction.options.getString('content');
-				await interaction.deferReply();
 				if (content.length > 100) {
 					return await interaction.editReply({
 						content: '🚫 You have way too many characters to fit in this meme.',
 						ephemeral: true,
 					});
 				}
-				writeMeme(type, content)
+				writeMeme(type, content, 20, 4)
 					.then(async (meme) => {
 						await interaction.editReply({ files: [meme] });
 					})
 					.catch((error) => console.log(error));
-			} catch (err) {
-				console.log(err);
-			}
-		} else if (type === 'vibrator') {
-			try {
+			} else if (type === 'vibrator') {
 				const content = interaction.options.getString('content');
-				await interaction.deferReply();
 				if (content.length > 100) {
 					return await interaction.editReply({
 						content: '🚫 You have way too many characters to fit in this meme.',
 						ephemeral: true,
 					});
 				}
-				writeMeme(type, content)
+				writeMeme(type, content, 1, 4)
 					.then(async (meme) => {
 						await interaction.editReply({ files: [meme] });
 					})
 					.catch((error) => console.log(error));
-			} catch (err) {
-				console.log(err);
-			}
-		} else {
-			try {
-				await interaction.reply({
+			} else if (type === 'sirtat') {
+				const content = interaction.options.getString('content');
+				if (content.length > 100) {
+					return await interaction.editReply({
+						content: '🚫 You have way too many characters to fit in this meme.',
+						ephemeral: true,
+					});
+				}
+				writeMeme(type, content, 20, 225)
+					.then(async (meme) => {
+						await interaction.editReply({ files: [meme] });
+					})
+					.catch((error) => console.log(error));
+			} else {
+				await interaction.editReply({
 					ephemeral: true,
 					content: 'This meme template does not exist',
 				});
-			} catch (err) {
-				console.log(err);
 			}
+		} catch (err) {
+			console.log(err);
 		}
 	},
 };
 
-const writeMeme = async (type, content) => {
+const writeMeme = async (type, content, x, y) => {
 	let image = await Jimp.read(`${process.cwd()}/commands/memes/${type}.jpg`);
 
-	let x = 1;
-	let y = 4;
 	let maxWidth = 400;
 	let maxHeight = 100;
 	let memeFont = Jimp.FONT_SANS_32_BLACK;
-	if (type === 'vibrator') memeFont = Jimp.FONT_SANS_16_BLACK;
-	if (type === 'wallace') {
-		x = 2;
-		y = 0;
-		maxWidth = 300;
-	}
+
 	await Jimp.loadFont(memeFont)
 		.then((font) => {
 			image.print(
@@ -116,6 +115,7 @@ const writeMeme = async (type, content) => {
 				{
 					text: content,
 					alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+					alignmentY: Jimp.VERTICAL_ALIGN_TOP,
 				},
 				maxWidth,
 				maxHeight
